7829a96017b5878fe3197559efafa7088b22ed61
Wojciech Rybakiewicz <wojtek.rybakiewicz@gmail.com>
2022-05-31 20:06:10 +0200

    get meme by id

---
+++ getMemeById/index.js
@@ -0,0 +1,90 @@
+require("dotenv").config()
+const {Pool} = require('pg')
+const {rows} = require("pg/lib/defaults");
+
+var dbConfig = {
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+};
+
+const pool = new Pool(dbConfig)
+
+let client;
+
+async function query(query, value) {
+    if(client === undefined) {
+        await initializeDbClient()
+    }
+    let res
+    try {
+        res = await client.query(query, value)
+    } catch (e) {
+        console.log("Error querying")
+        throw e
+    }
+    return res
+}
+
+const initializeDbClient = async () => {
+    try {
+        client = await pool.connect()
+    } catch (e) {
+        console.log("Error initializing client")
+        throw e
+    }
+}
+
+let response;
+
+exports.handler = async (event, context) => {
+    try {
+        console.log(event)
+        const queryParams = event.queryStringParameters
+        const address = queryParams.address
+        const id = queryParams.id
+        console.log("Querying: " + address + " id: " + id)
+
+        const queryString = `
+            SELECT id, title, link, vote_up_count, vote_down_count, competition_id
+            FROM memes
+            WHERE is_blocked = false
+            AND id = $1
+            LIMIT 1
+        `
+        const rowsMemesPromise = query(queryString, [id])
+        const rowsMemes = (await rowsMemesPromise).rows
+
+        if (rowsMemes.length === 0) {
+            return;
+        }
+
+        let meme = rowsMemes[0]
+        if(address) {
+            console.log("Querying votes for address: " + address)
+            const queryString = `SELECT meme_id, vote_up, vote_down FROM votes WHERE address = $1 AND meme_id =$2`
+            const votes = (await query(queryString, [address, id])).rows
+            console.log(votes)
+            if(votes.length > 0) {
+                meme["votedUp"] = votes[0].vote_up
+                meme["votedDown"] = votes[0].vote_down
+            }
+        }
+
+        console.log(JSON.stringify(meme))
+        response = {
+            'statusCode': 200,
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,GET"
+            },
+            "body": JSON.stringify({meme: meme}),
+        }
+        return response
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+};
---
+++ getMemeById/tests/unit/test-handler.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const app = require('../../index.js');
+const chai = require('chai');
+const expect = chai.expect;
+var event, context;
+
+describe('Tests index', function () {
+    it('verifies successful response', async () => {
+        event = {
+            queryStringParameters: {
+                id: 1,
+                address: '0x26ea8aaf2028ecb261e36b61e6a21b03bd229c77'
+            }
+        }
+        const result = await app.handler(event, context)
+
+        expect(result).to.be.an('object');
+        expect(result.statusCode).to.equal(200);
+        expect(result.body).to.be.an('string');
+
+        let response = JSON.parse(result.body);
+        console.log(response)
+
+        expect(response.meme).to.be.an('object');
+    });
+    it('verifies successful response without address', async () => {
+        event = {
+            queryStringParameters: {
+                id: 1,
+            }
+        }
+        const result = await app.handler(event, context)
+
+        expect(result).to.be.an('object');
+        expect(result.statusCode).to.equal(200);
+        expect(result.body).to.be.an('string');
+
+        let response = JSON.parse(result.body);
+        console.log(response)
+
+        expect(response.meme).to.be.an('object');
+    });
+});
